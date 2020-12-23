53ab1f20e0f5460f11317d2fd7f3651f0057b19a
Fernando Delago <39306731+fernandodelago@users.noreply.github.com>
2020-12-23 18:54:10 -0300

    Remove vogais com acentos e "ç"

---
+++ remove_acentos.py
@@ -0,0 +1,26 @@
+from pyspark.sql.functions import col, regexp_replace
+
+# substitui vogais com acentos por sem acentos
+# substitui "Ç" por "C"
+# caracteres especiais ficarão com valor '#'
+
+acentos_portugues = [
+    (u'á', 'a'), (u'Á', 'A'),
+    (u'â', 'a'), (u'Â', 'A'),
+    (u'à', 'a'), (u'À', 'A'),
+    (u'é', 'e'), (u'É', 'E'),
+    (u'ê', 'e'), (u'Ê', 'E'),
+    (u'í', 'i'), (u'Í', 'I'),
+    (u'ò', 'o'), (u'Ó', 'O'),
+    (u'ô', 'o'), (u'Ô', 'O'),
+    (u'ú|ü', 'u'), (u'Ú|Ű', 'U'),
+    (u'ñ', 'n'),
+    (u'ç', 'c'), (u'Ç', 'C'),
+    ('[^\x00-\x7F]', '#') 
+]
+
+def remove_acentos(column):
+    r = col(column)
+    for a, b in acentos_portugues:
+        r = regexp_replace(r, a, b)
+    return r.alias('remove_acentos(' + column + ')')
