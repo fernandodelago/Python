1279d50db78e5710d873fcb47d78a931ed5d867f
Fernando Delago <39306731+fernandodelago@users.noreply.github.com>
2021-01-13 14:47:13 -0300

    Atualização
    Incluindo tratamento de timezone.
    (EN) Apply timezone configuration.

--- mostra_hora.py
+++ mostra_hora.py
@@ -1,7 +1,15 @@
 #! python 3
 
 def mostra_hora():
-    '''Função simples para imprimir a hora na tela'''
-    from datetime import datetime
-    agora = datetime.now()
-    print(agora.strftime('%Y-%m-%d %H:%M:%S'))
+    '''
+    Função simples para imprimir a hora na tela.
+    Fuso_horário utilizado - Brazil/Sao_Paulo
+    --
+    (EN)Simple function to show date and time.
+    Timezone used - Brazil/Sao_paulo
+    '''
+    from datetime import datetime, timedelta, timezone
+    diferenca = timedelta(hours = -3)
+    fuso_horario = timezone(diferenca)
+    dt_hr_agora = datetime.now(fuso_horario)
+    print(dt_hr_agora.strftime('%Y-%m-%d %H:%M:%S'))
