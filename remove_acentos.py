153cc214677e2dc43c3570f741f429933f41b4e2
Fernando Delago <39306731+fernandodelago@users.noreply.github.com>
2024-02-20 15:39:23 +0000

    remover acentos de colunas

--- remove_acentos.py
+++ remove_acentos.py
@@ -1,10 +1,14 @@
 def udf_col_name_standard(df_name):
     '''
     Format all columns of dataframe in lowercase and remove special characters for "_".
+    lib required: pip install unidecode
     Input.: Spark Dataframe
     Output: Spark Dataframe
     Example how to use:
+    from unidecode import unidecode
     df_result = udf_col_name_standard(df_original_data)
     '''
     # library used: import re
-    return df_name.toDF(*[re.sub("[^0-9a-zA-Z]+",'_', item_column).lower() for item_column in df_name.columns])
+    new_columns = [unidecode(col_name).lower().replace(' ', '_') for col_name in df.columns]
+    renamed_df = df.toDF(*new_columns)
+    return renamed_df
